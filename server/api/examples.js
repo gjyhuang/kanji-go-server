474f7b288b1a7a59bef20348366f0b01e8e8a2b1
gjyhuang <grace.jy.huang@gmail.com>
2020-03-21 23:18:58 -0400

    add: get by id, put, and delete routes for examples

--- server/api/examples.js
+++ server/api/examples.js
@@ -5,7 +5,7 @@ module.exports = router;
 
 router.get("/", async (req, res, next) => {
   try {
-    const exampless = await Example.findAll({
+    const examples = await Example.findAll({
       include: [
         {
           model: Card,
@@ -13,7 +13,7 @@ router.get("/", async (req, res, next) => {
         }
       ]
     });
-    res.json(exampless);
+    res.json(examples);
   } catch (err) {
     next(err);
   }
@@ -30,6 +30,47 @@ router.post("/", async (req, res, next) => {
       tags
     });
     res.status(200).json(newExample);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:exampleId", async (req, res, next) => {
+  try {
+    const example = await Example.findByPk(req.params.exampleId, {
+      include: [{ model: Card }]
+    });
+    res.json(example);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put("/:exampleId", async (req, res, next) => {
+  try {
+    const { exampleId } = req.params;
+    const exampleToUpdate = await Example.findByPk(exampleId);
+    const { audio, japanese, english, tags } = req.body;
+    const updatedData = {
+      audio,
+      japanese,
+      english,
+      tags
+    };
+    exampleToUpdate.update(updatedData);
+    res.status(200).json(exampleToUpdate);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete("/:exampleId", async (req, res, next) => {
+  try {
+    const { exampleId } = req.params;
+    await Example.destroy({
+      where: { id: exampleId }
+    });
+    res.sendStatus(200);
   } catch (err) {
     next(err);
   }
